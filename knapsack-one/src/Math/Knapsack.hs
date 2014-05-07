97f128c9ce5571a56876f94ae8388e237c02f632
Tom Hulihan <hulihan.tom159@gmail.com>
2014-05-07 14:38:26 -0400

    Add the Knapsack parser.

--- knapsack-one/src/Math/Knapsack.hs
+++ knapsack-one/src/Math/Knapsack.hs
@@ -1,4 +1,6 @@
 module Math.Knapsack ( module Math.Knapsack.Data
+                     , module Math.Knapsack.Parser
                      ) where
 
 import Math.Knapsack.Data
+import Math.Knapsack.Parser
