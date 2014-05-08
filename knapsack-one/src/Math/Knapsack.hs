ea6daef25958642bf9d4cad93e3f164e5b15ecdc
Tom Hulihan <hulihan.tom159@gmail.com>
2014-05-07 22:02:53 -0400

    WIP Algorithm

--- knapsack-one/src/Math/Knapsack.hs
+++ knapsack-one/src/Math/Knapsack.hs
@@ -1,6 +1,8 @@
-module Math.Knapsack ( module Math.Knapsack.Data
+module Math.Knapsack ( module Math.Knapsack.Algorithm
+                     , module Math.Knapsack.Data
                      , module Math.Knapsack.Parser
                      ) where
 
+import Math.Knapsack.Algorithm
 import Math.Knapsack.Data
 import Math.Knapsack.Parser
---
+++ knapsack-one/src/Math/Knapsack/Algorithm.hs
@@ -0,0 +1,1 @@
+module Math.Knapsack.Algorithm where
