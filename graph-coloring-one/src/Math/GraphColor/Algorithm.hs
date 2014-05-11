60d9dfbc3b1fda93510a94a40d0f25358390b70f
Todd Lunter <tlunter@gmail.com>
2014-05-11 18:42:33 -0400

    Add time cutoff, make into generators and faster conflicts

--- graph-coloring-one/src/Math/GraphColor/Algorithm.hs
+++ graph-coloring-one/src/Math/GraphColor/Algorithm.hs
@@ -5,8 +5,9 @@ import Control.Monad.Reader
 import Control.Monad.State
 import Math.GraphColor.Data
 import Pipes
---import System.Hourglass
---import Data.Hourglass
+import System.Hourglass
+import Data.Hourglass
+import Data.Monoid
 
 
 -- The GraphColorState is a list of nodes with a corresponding color, and the
@@ -21,6 +22,7 @@ data GraphColorState =
 -- the corresponding edges between nodes
 data GraphColorEnv =
     GraphColorEnv { edges     :: [Edge]
+                  , endTime   :: Elapsed
                   } deriving (Eq, Show)
 
 type GraphColor = ReaderT GraphColorEnv (StateT GraphColorState IO)
@@ -30,49 +32,66 @@ defaultState = GraphColorState { nodeColors = []
                                , conflicts  = maxBound
                                }
 
-runGraphColor :: [Edge] -> GraphColor a -> IO (a, GraphColorState)
-runGraphColor es action = flip runStateT defaultState . runReaderT action $ GraphColorEnv es
+runTime :: TimeDiff
+runTime = mempty { timeDiffMinutes = 10 }
 
-subsequences :: Int -> Int -> Producer [Node] GraphColor ()
-subsequences numColors numNodes =
-    let colorList = [1..numColors]
-        nodeList  = [0..numNodes]
-    in  nodeLoop colorList nodeList
-
-colorLoop :: Monad m => Int -> [Int] -> Producer Node m ()
-colorLoop n cs = for (each cs) $ yield . Node n
-
-nodeLoop :: Monad m => [Int] -> [Int] -> Producer [Node] m ()
-nodeLoop cs =
-    let loop [] = yield []
-        loop (n:ns) =
-            for (colorLoop n cs) $ \node ->
-                for (loop ns) $ \nodes ->
-                    yield $ node : nodes
-    in  loop
-
-checkConflict :: Consumer [Node] GraphColor ()
-checkConflict = do
-    es <- asks edges
-    nodes <- await
-    unless (null nodes || (color (head nodes) > 1)) $ do
-        edgeCs <- lift $ forM es $ \e -> do
-            let left = nodes !! a e
-                right = nodes !! b e
-            return . fromEnum $ color left == color right
-        let cs = sum edgeCs
-        originalCs <- gets conflicts
-        when (originalCs > cs) . put $ GraphColorState nodes cs
-        unless (cs == 0) checkConflict
+runGraphColor :: [Edge] -> Elapsed -> GraphColor a -> IO (a, GraphColorState)
+runGraphColor es et action = flip runStateT defaultState . runReaderT action $ GraphColorEnv es et
 
 tryListCombinations :: Int -> Int -> GraphColor ()
 tryListCombinations nc nn = runEffect $
-    subsequences nc nn >-> checkConflict
+    subsequences nc nn >-> filterTime >-> consume
+
+subsequences :: Int -> Int -> Producer [Node] GraphColor ()
+subsequences cs ns =
+    let loop n =
+            if n == ns
+            then yield []
+            else for (colorLoop n cs) $ \node ->
+                    for (loop (n + 1)) $ \nodes ->
+                        yield $ node : nodes
+    in  loop 0
+
+colorLoop :: Monad m => Int -> Int -> Producer Node m ()
+colorLoop n maxColor =
+    let loop c =
+            unless (c > maxColor) $ do
+                yield $ Node n c
+                loop (c + 1)
+    in loop 1
+
+consume :: Consumer [Node] GraphColor ()
+consume = do
+    es <- asks edges
+    nodes <- await
+    unless (null nodes || (color (head nodes) > 1)) $ do
+        originalCs <- gets conflicts
+        let checkConflicts' acc e = acc + fromEnum (color left == color right)
+                where left  = nodes !! a e
+                      right = nodes !! b e
+            checkConflicts acc []     = acc
+            checkConflicts acc (e:es') = if acc > originalCs
+                                        then acc
+                                        else checkConflicts (checkConflicts' acc e) es'
+        let cs = checkConflicts 0 es
+        when (originalCs > cs) . put $ GraphColorState nodes cs
+        unless (cs == 0) consume
+
+-- Don't compute the computation run if time has run out.
+filterTime :: Pipe a a GraphColor ()
+filterTime =
+    let loop eTime = do
+            elt <- await
+            cTime <- liftIO timeCurrent
+            when (eTime > cTime) $ yield elt >> loop eTime
+    in  asks endTime >>= loop
 
 findBest :: Int -> Int -> [Edge] -> IO [Node]
 findBest nc nn es = do
     putStrLn "Starting to build combinations"
-    (_, s) <- runGraphColor es (tryListCombinations nc nn)
+    currentT <- timeCurrent
+    let endT = timeAdd currentT runTime
+    (_, s) <- runGraphColor es endT (tryListCombinations nc nn)
     putStrLn $ "Finished running, only " ++ show (conflicts s) ++ " conflicts"
     putStrLn $ "Colors: " ++ show (nodeColors s)
     return $ nodeColors s
