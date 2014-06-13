446136317566ccb5e84957bc100db6f1dac51dea
Tom Hulihan <hulihan.tom159@gmail.com>
2014-06-12 20:22:41 -0400

    knapsack-five scaffold

---
+++ knapsack-five/src/Main.hs
@@ -0,0 +1,27 @@
+module Main (main) where
+
+import Math.Knapsack as K
+import System.Environment
+
+main :: IO ()
+main = do
+    args <- getArgs
+    if null args then putStrLn "Missing filename"
+    else do
+        let filename = head args
+        putStrLn "Parsing file"
+        possibleKnapsack <- parseFile filename
+        objs <- either parseError perform possibleKnapsack
+
+        putStrLn "------------------------------------------------"
+        putStrLn $ "Total value: " ++ (show . sum $ map value objs)
+        putStrLn $ "Total cost: " ++ (show . sum $ map cost objs)
+        mapM_ print objs
+
+perform :: (Int, Int, [Object]) -> IO [Object]
+perform (_, c, os) = do
+    putStrLn $ "Looking for set of objects less than " ++ show c
+    return $ findBest os c
+
+parseError :: String -> IO [Object]
+parseError e = putStrLn e >> return []
